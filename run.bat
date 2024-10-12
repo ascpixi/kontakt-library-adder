190dc612ea0fd30044f277250756976501c4679f
ascpixi <44982772+ascpixi@users.noreply.github.com>
2024-10-13 01:31:14 +0200

    fix: detect non-admin environment; fix cwd for admin

--- run.bat
+++ run.bat
@@ -1,4 +1,20 @@
-@pip install -r requirements.txt
-@cls
-@python add-library.py [gui]
-@pause
+@echo off
+
+:: windows does this silly thing where it navigates away from the current directory to C:/Windows/system32
+:: when the script is ran as administrator. ensure we're in the right directory.
+setlocal enableextensions
+cd /d "%~dp0"
+
+:: check for admin permissions (so that we can modify HKEY_LOCAL_MACHINE)
+net session >nul 2>&1
+if not %errorlevel% == 0 (
+	echo [-] this script requires administrator permissions.
+	echo [-] run it again as administrator to properly launch it.
+	pause
+	exit
+)
+
+pip install -r requirements.txt
+cls
+python add-library.py [gui]
+pause
